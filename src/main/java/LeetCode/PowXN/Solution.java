515dcf8f8a6187c1cba60806b511fd6fe67284f0
bijh <bijh@tsingyun.net>
2020-04-04 21:03:52 +0800

    "n次方求解"

--- src/main/java/LeetCode/PowXN/Solution.java
+++ src/main/java/LeetCode/PowXN/Solution.java
@@ -1,6 +1,5 @@
-package Amazon.PowXN;
-import java.util.ArrayList;
-import java.util.List;
+package LeetCode.PowXN;
+
 public class Solution {
     double powx(double x, int n) {
         if (n < 0) {
@@ -12,6 +11,7 @@ public class Solution {
         double r = powx(x, n / 2);
         return (n & 1) == 1 ? x * r * r : r * r;
     }
+
     public double powx1(double x, int n) {
         //当n是负数的情况
         if (n < 0) {
@@ -21,12 +21,26 @@ public class Solution {
         }
         double ans = 1, tmp = x;
         while (n != 0) {
-            if ((n&1)==1) {
+            if ((n & 1) == 1) {
                 ans *= tmp;
             }
             n >>>= 1;
             tmp *= tmp;
         }
         return ans;
+    }
+
+    public double powx2(double x, int n) {
+        double result = 1;
+        //防止溢出
+        long N = (long) Math.abs(n);
+        while (N != 0) {
+            if ((n & 1) == 1) {
+                result *= x;
+            }
+            x *= x;
+            N >>= 1;
+        }
+        return n > 0 ? result : 1 / result;
     }
 }
