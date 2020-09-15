1c938ff43dc99d4de92e7b32cd42cd0e58cef677
bijh <bijh@tsingyun.net>
2020-09-15 15:31:13 +0800

    "字典树"

--- src/main/java/Amazon/BalancedBinaryTree/Solution.java
+++ src/main/java/Amazon/BalancedBinaryTree/Solution.java
@@ -1,6 +1,6 @@
 package Amazon.BalancedBinaryTree;
+
 /**
- * Email: clickgwas@gmail.com
  * https://www.jiuzhang.com/solutions/balanced-binary-tree/
  * 给定一个二叉树,确定它是高度平衡的。对于这个问题,一棵高度平衡的二叉树的定义是：
  * 一棵二叉树中每个节点的两个子树的深度相差不会超过1。
@@ -8,26 +8,41 @@ package Amazon.BalancedBinaryTree;
 class TreeNode {
     public int val;
     public TreeNode left, right;
+
     public TreeNode(int val) {
         this.val = val;
         this.left = this.right = null;
     }
+
 }
+
 public class Solution {
+
+    /**
+     * @param root: The root of binary tree.
+     * @return: True if this Binary tree is Balanced, or false.
+     */
+
+    boolean isBalanced = true;
+
     public boolean isBalanced(TreeNode root) {
-        return maxDepth(root) != -1;
+        dfs(root);
+        return isBalanced;
     }
 
-    private int maxDepth(TreeNode root) {
+    private int dfs(TreeNode root) {
         if (root == null) {
             return 0;
         }
 
-        int left = maxDepth(root.left);
-        int right = maxDepth(root.right);
-        if (left == -1 || right == -1 || Math.abs(left-right) > 1) {
-            return -1;
+        int leftDepth = dfs(root.left);
+        int rightDepth = dfs(root.right);
+
+        if (Math.abs(leftDepth - rightDepth) > 1) {
+            isBalanced = false;
         }
-        return Math.max(left, right) + 1;
+
+        return Math.max(leftDepth, rightDepth) + 1;
     }
+
 }
