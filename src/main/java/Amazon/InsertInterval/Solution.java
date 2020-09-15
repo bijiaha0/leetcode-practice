1117b4f172aba42d283003712751a040eca66d28
bijh <bijh@tsingyun.net>
2020-09-15 22:52:09 +0800

    "数位分离"

--- src/main/java/Amazon/InsertInterval/Solution.java
+++ src/main/java/Amazon/InsertInterval/Solution.java
@@ -1,9 +1,11 @@
 package Amazon.InsertInterval;
+
 import java.util.ArrayList;
 import java.util.List;
+
 /**
  * 往往会将区间按照左端点从小到大排个序
- *
+ * <p>
  * Author: bijiaha0
  * Date: 2019-02-23
  * Time: 2:13 AM
@@ -13,20 +15,24 @@ import java.util.List;
  * (2, 5) into [(1,2), (5,9)]
  * Output:
  * [(1,9)]
- *
+ * <p>
  * Input:
  * (3, 4) into [(1,2), (5,9)]
  * Output:
  * [(1,2), (3,4), (5,9)]
+ * <p>
+ * 使用 Merge Interval
  */
-class Interval{
+class Interval {
     int start;
     int end;
-    Interval(int start, int end){
+
+    Interval(int start, int end) {
         this.start = start;
         this.end = end;
     }
 }
+
 public class Solution {
     public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
         List<Interval> ans = new ArrayList<>();
