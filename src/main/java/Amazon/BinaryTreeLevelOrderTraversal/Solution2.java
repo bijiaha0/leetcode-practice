00cfc0fb7c7d7cce8c7c06f9e543ab008ad345cb
bijh <bijh@tsingyun.net>
2020-09-15 16:39:45 +0800

    "层序遍历"

--- src/main/java/Amazon/BinaryTreeLevelOrderTraversal/Solution2.java
+++ src/main/java/Amazon/BinaryTreeLevelOrderTraversal/Solution2.java
@@ -1,18 +1,35 @@
 package Amazon.BinaryTreeLevelOrderTraversal;
+
 import java.util.ArrayList;
 import java.util.List;
+
 public class Solution2 {
 
     public List<List<Integer>> levelOrder(TreeNode root) {
+
         List<List<Integer>> res = new ArrayList<>();
+
         dfs(root, 0, res);
+
         return res;
+
     }
-    void dfs(TreeNode root, int level, List<List<Integer>> res){
-        if (root==null) return;
-        if(res.size()<level+1) res.add(new ArrayList<>());
+
+    void dfs(TreeNode root, int level, List<List<Integer>> res) {
+
+        if (root == null) {
+            return;
+        }
+
+        if (res.size() < level + 1) {
+            res.add(new ArrayList<>());
+        }
+
         res.get(level).add(root.val);
-        dfs(root.left,level+1,res);
-        dfs(root.right,level+1,res);
+
+        dfs(root.left, level + 1, res);
+        dfs(root.right, level + 1, res);
+
     }
+
 }
