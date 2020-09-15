bdeea3dbf16b9b911bf72eecde287a484eb7cae3
bijh <bijh@tsingyun.net>
2020-09-15 22:44:21 +0800

    "数位分离"

--- src/main/java/Amazon/MergeIntervals/Solution.java
+++ src/main/java/Amazon/MergeIntervals/Solution.java
@@ -1,40 +1,32 @@
 package Amazon.MergeIntervals;
+
 import java.util.*;
+
 /**
- * Author: bijiaha0
- * Date: 2019-02-23
- * Time: 1:45 AM
- * Email: clickgwas@gmail.com
  * https://www.lintcode.com/problem/merge-intervals/description
  */
 /*
-* 区间左端点从小到大排个序，从左往右扫一遍
-* 不能合并---> 直接下一个
-* 能合并 ---> 就合并
-* */
-class Interval{
+ * 区间左端点从小到大排个序，从左往右扫一遍
+ * 不能合并---> 直接下一个
+ * 能合并 ---> 就合并
+ * */
+class Interval {
     int start;
     int end;
-    Interval(int start, int end){
+
+    Interval(int start, int end) {
         this.start = start;
         this.end = end;
     }
 }
+
 public class Solution {
     public List<Interval> merge(List<Interval> intervals) {
         List<Interval> ans = new ArrayList<>();
         if (intervals == null) {
             return ans;
         }
-        //intervals.sort(Comparator.comparing(i -> i.start));  //lambda 匿名函数：输入i  返回i.start
-        //排序
-        Collections.sort(intervals, new Comparator<Interval>() {
-            @Override
-            public int compare(Interval o1, Interval o2) {
-                return o1.start - o2.start;
-            }
-        });
-        intervals.sort(Comparator.comparing(o->o.start));
+        intervals.sort(Comparator.comparing(i -> i.start));  //lambda 匿名函数：输入i  返回i.start
         Interval last = null;
         for (Interval item : intervals) {
             if (last == null || last.end < item.start) {
