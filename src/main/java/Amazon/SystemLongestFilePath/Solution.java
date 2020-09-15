c890e8fd4c72d56fe5c2477a2f94f3bd733e6847
bijh <bijh@tsingyun.net>
2020-09-15 11:39:42 +0800

    "二进制相加"

--- src/main/java/Amazon/SystemLongestFilePath/Solution.java
+++ src/main/java/Amazon/SystemLongestFilePath/Solution.java
@@ -1,14 +1,19 @@
 package Amazon.SystemLongestFilePath;
 
+import java.util.Stack;
+
 /**
  * https://www.jiuzhang.com/solutions/system-longest-file-path/
- *
+ * <p>
  * "dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext"
- *
+ * <p>
+ * “dir/subdir2/subsubdir2/file2.ext”---->32
+ * <p>
+ * "dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext"
  * dir
- * 	subdir1
- * 	subdir2
- * 		file.ext
+ * subdir1
+ * subdir2
+ * file.ext
  */
 public class Solution {
 
@@ -19,18 +24,47 @@ public class Solution {
         }
 
         int ans = 0;
-        int[] sum = new int[input.length() + 1];//前缀和数组
+        int[] sum = new int[input.length() + 1];
 
         for (String line : input.split("\n")) {
-            int level = line.lastIndexOf('\t') + 2;//\t的个数
-            int len = line.length() - (level - 1);//长度的计算应该是：剩余字符串加上一个斜杠的长度
+            int level = line.lastIndexOf('\t') + 2;
+            int len = line.length() - (level - 1); //level - 1 = \t的长度
             if (line.contains(".")) {
                 ans = Math.max(ans, sum[level - 1] + len);
             } else {
-                sum[level] = sum[level - 1] + len + 1;//如果是文件夹的话，加内个斜杠
+                sum[level] = sum[level - 1] + len + 1; //remove \t ,add /
             }
         }
+
         return ans;
+    }
+
+    public int lengthLongestPath2(String input) {
+        int maxLen = 0;
+        if (input == null || input.length() == 0) {
+            return maxLen;
+        }
+        Stack<Integer> stack = new Stack();
+        stack.push(0);
+        String[] inputArr = input.split("\n");
+        for (String inputStr : inputArr) {
+
+            int level = inputStr.lastIndexOf("\t") + 1;
+
+            while (level + 1 < stack.size()) {
+                stack.pop();
+            }
+
+            int newLen = stack.peek() + inputStr.length() - level + 1; //remove \t ,add /
+            stack.push(newLen);
+
+            if (inputStr.contains(".")) {
+                maxLen = Math.max(maxLen, newLen - 1);
+            }
+
+        }
+
+        return maxLen;
     }
 
 }
