21fd839d8deede2497ea23f586d419d2e4a214a7
bijh <bijh@tsingyun.net>
2020-09-15 09:44:49 +0800

    "二进制相加"

--- src/main/java/Amazon/SystemLongestFilePath/Solution.java
+++ src/main/java/Amazon/SystemLongestFilePath/Solution.java
@@ -2,15 +2,25 @@ package Amazon.SystemLongestFilePath;
 
 /**
  * https://www.jiuzhang.com/solutions/system-longest-file-path/
- * 模拟一个栈
+ *
+ * "dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext"
+ *
+ * dir
+ * 	subdir1
+ * 	subdir2
+ * 		file.ext
  */
 public class Solution {
+
     public int lengthLongestPath(String input) {
+
         if (input.length() == 0) {
             return 0;
         }
+
         int ans = 0;
         int[] sum = new int[input.length() + 1];//前缀和数组
+
         for (String line : input.split("\n")) {
             int level = line.lastIndexOf('\t') + 2;//\t的个数
             int len = line.length() - (level - 1);//长度的计算应该是：剩余字符串加上一个斜杠的长度
@@ -23,28 +33,4 @@ public class Solution {
         return ans;
     }
 
-
-    /*
-     * @param input an abstract file system
-     * @return return the length of the longest absolute path to file
-     */
-    public int lengthLongestPath2(String input) {
-
-        if (input.length() == 0) {
-            return 0;
-        }
-        int ans = 0;
-        int[] sum = new int[input.length() + 1];
-
-        for (String line : input.split("\n")) {
-            int level = line.lastIndexOf('\t') + 2;
-            int len = line.length() - (level - 1);
-            if (line.contains(".")) {
-                ans = Math.max(ans, sum[level - 1] + len);
-            } else {
-                sum[level] = sum[level - 1] + len + 1;
-            }
-        }
-        return ans;
-    }
 }
