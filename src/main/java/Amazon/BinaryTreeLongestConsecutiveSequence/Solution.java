d678838e33ecbf166c9e15d1938d5c99b1f41155
bijh <bijh@tsingyun.net>
2020-09-15 16:50:53 +0800

    "最长连续序列"

--- src/main/java/Amazon/BinaryTreeLongestConsecutiveSequence/Solution.java
+++ src/main/java/Amazon/BinaryTreeLongestConsecutiveSequence/Solution.java
@@ -1,23 +1,31 @@
 package Amazon.BinaryTreeLongestConsecutiveSequence;
 
 /**
- * Email: clickgwas@gmail.com
  * https://www.jiuzhang.com/solutions/binary-tree-longest-consecutive-sequence/
+ * 二叉树最长连续序列
  */
 class TreeNode {
+
     public int val;
     public TreeNode left, right;
+
     public TreeNode(int val) {
         this.val = val;
         this.left = this.right = null;
     }
+
 }
+
 public class Solution {
+
     public int longestConsecutive(TreeNode root) {
+
         return helper(root, null, 0);
+
     }
 
     private int helper(TreeNode root, TreeNode parent, int lengthWithoutRoot) {
+
         if (root == null) {
             return 0;
         }
@@ -26,5 +34,6 @@ public class Solution {
         int left = helper(root.left, root, length);
         int right = helper(root.right, root, length);
         return Math.max(length, Math.max(left, right));
+
     }
 }
