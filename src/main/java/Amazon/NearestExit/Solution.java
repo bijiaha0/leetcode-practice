5e152bbe2d0d6ee4f183e0429028fb2b48d7741a
bijh <bijh@tsingyun.net>
2020-09-16 14:43:32 +0800

    "bfs,宽度优先搜索"

--- src/main/java/Amazon/NearestExit/Solution.java
+++ src/main/java/Amazon/NearestExit/Solution.java
@@ -1,6 +1,27 @@
 package Amazon.NearestExit;
+
 import java.util.LinkedList;
 import java.util.Queue;
+
+/**
+ * 您将获得一个使用这三个可能值初始化的 m×n 2D 网格。
+ * -1 - 墙壁或障碍物。
+ * 0 - 门。
+ * INF - Infinity是一个空房间。我们使用值 2 ^ 31 - 1 = 2147483647 来表示INF，您可以假设到门的距离小于 2147483647。
+ * 在代表每个空房间的网格中填入到距离最近门的距离。如果不可能到达门口，则应填入 INF。
+ * <p>
+ * <p>
+ * 2D网络为：
+ * INF  -1  0  INF
+ * INF INF INF  -1
+ * INF  -1 INF  -1
+ * 0  -1 INF INF
+ * 答案为：
+ * 3  -1   0   1
+ * 2   2   1  -1
+ * 1  -1   2  -1
+ * 0  -1   3   4
+ */
 public class Solution {
     static final int INF = Integer.MAX_VALUE;
     int n, m;
