dc35259cbc72ef64210026efcb506e7afda7c236
bijh <bijh@tsingyun.net>
2020-09-16 16:36:08 +0800

    "数位分离"

--- src/main/java/LeetCode/TopKFrequentElements/Solution.java
+++ src/main/java/LeetCode/TopKFrequentElements/Solution.java
@@ -1,26 +1,25 @@
 package LeetCode.TopKFrequentElements;
+
 import java.util.*;
+
 /**
- * Author: bijiaha0
- * Date: 2019-04-02
- * Time: 23:19
- * Email: clickgwas@gmail.com
  * https://leetcode.com/problems/top-k-frequent-elements/solution/
  */
 public class Solution {
     public List<Integer> topKFrequent(int[] nums, int k) {
+
         // build hash map : character and how often it appears
         HashMap<Integer, Integer> count = new HashMap();
-        for (int n: nums) {
+        for (int n : nums) {
             count.put(n, count.getOrDefault(n, 0) + 1);
         }
 
         // init heap 'the less frequent element first'
-        PriorityQueue<Integer> heap =
-                new PriorityQueue<Integer>((n1, n2) -> count.get(n1) - count.get(n2));
+        //初始化小跟堆
+        PriorityQueue<Integer> heap = new PriorityQueue<Integer>((n1, n2) -> count.get(n1) - count.get(n2));
 
         // keep k top frequent elements in the heap
-        for (int n: count.keySet()) {
+        for (int n : count.keySet()) {
             heap.add(n);
             if (heap.size() > k)
                 heap.poll();
@@ -28,9 +27,12 @@ public class Solution {
 
         // build output list
         List<Integer> top_k = new LinkedList();
-        while (!heap.isEmpty())
+        while (!heap.isEmpty()) {
             top_k.add(heap.poll());
+        }
+
         Collections.reverse(top_k);
+
         return top_k;
     }
 }
