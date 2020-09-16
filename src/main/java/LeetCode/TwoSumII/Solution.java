68694a88b32c434ee7ff7e46ce7561a24dbc82ed
bijh <bijh@tsingyun.net>
2020-09-16 16:29:15 +0800

    "数位分离"

--- src/main/java/LeetCode/TwoSumII/Solution.java
+++ src/main/java/LeetCode/TwoSumII/Solution.java
@@ -4,9 +4,9 @@ package LeetCode.TwoSumII;
  * @author bijh@tsingyun.net
  * @date 2020/4/4 3:35 PM
  * https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
- * id = 167
  */
 public class Solution {
+
     public int[] twoSum(int[] numbers, int target) {
         int i = 0, j = numbers.length - 1;
         while (i < j) {
@@ -20,4 +20,5 @@ public class Solution {
         }
         return new int[]{-1, -1};
     }
+
 }
