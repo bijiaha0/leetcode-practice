e43e7a5eaaa088433953ebd85ed322f6d2e480bf
bijh <bijh@tsingyun.net>
2020-09-16 08:30:12 +0800

    "数位分离"

---
+++ src/main/java/Amazon/BinaryTreeFlipping/Solution.java
@@ -0,0 +1,38 @@
+package Amazon.BinaryTreeFlipping;
+
+/**
+ * @author bijh@tsingyun.net
+ * @date 2020/9/16 8:27 AM
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+public class Solution {
+
+    TreeNode dfs(TreeNode cur) {
+        if (cur.left == null) {
+            return cur;
+        }
+        TreeNode newRoot = dfs(cur.left);
+        cur.left.right = cur;
+        cur.left.left = cur.right;
+        cur.left = null;            // important
+        cur.right = null;
+        return newRoot;
+    }
+
+    public TreeNode upsideDownBinaryTree(TreeNode root) {
+        // Write your code here
+        if (root == null) {
+            return null;
+        }
+        return dfs(root);
+    }
+}
