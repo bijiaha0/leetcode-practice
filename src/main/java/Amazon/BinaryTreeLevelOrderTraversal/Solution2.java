df9a20a5f6a4939c5d1429d173b23251e6e1dc47
bijh <bijh@tsingyun.net>
2020-09-16 08:36:03 +0800

    "数位分离"

--- src/main/java/Amazon/BinaryTreeLevelOrderTraversal/Solution2.java
+++ src/main/java/Amazon/BinaryTreeLevelOrderTraversal/Solution2.java
@@ -12,9 +12,7 @@ public class Solution2 {
     public List<List<Integer>> levelOrder(TreeNode root) {
 
         List<List<Integer>> res = new ArrayList<>();
-
         dfs(root, 0, res);
-
         return res;
 
     }
@@ -24,13 +22,10 @@ public class Solution2 {
         if (root == null) {
             return;
         }
-
         if (res.size() < level + 1) {
             res.add(new ArrayList<>());
         }
-
         res.get(level).add(root.val);
-
         dfs(root.left, level + 1, res);
         dfs(root.right, level + 1, res);
 
