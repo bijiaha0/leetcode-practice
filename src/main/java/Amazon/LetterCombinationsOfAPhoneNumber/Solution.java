0415e63cb69ac8fdd643cf4df813f0bb8fe97339
bijh <bijh@tsingyun.net>
2020-09-24 17:47:07 +0800

    "区间类问题"

--- src/main/java/Amazon/LetterCombinationsOfAPhoneNumber/Solution.java
+++ src/main/java/Amazon/LetterCombinationsOfAPhoneNumber/Solution.java
@@ -13,6 +13,7 @@ import java.util.List;
 public class Solution {
 
     public List<String> letterCombinations(String digits) {
+
         String[] phone = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
         List<String> ans = new ArrayList<>();
 
@@ -22,16 +23,20 @@ public class Solution {
 
         dfs(0, digits.length(), "", digits, phone, ans);
         return ans;
+
     }
 
     private void dfs(int x, int l, String str, String digits, String[] phone, List<String> ans) {
 
-        if (x == l) {//退出
+        //退出
+        if (x == l) {
             ans.add(str);
             return;
         }
 
-        int d = digits.charAt(x) - '0';//让字符变成真实的数字
+        //让字符变成真实的数字
+        int d = digits.charAt(x) - '0';
+
         //扩展
         for (char c : phone[d].toCharArray()) {
             dfs(x + 1, l, str + c, digits, phone, ans);
