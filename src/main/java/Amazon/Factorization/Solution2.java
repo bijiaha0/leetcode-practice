82e341a244d85bc43322b70f78903c4e3b26a9d5
bijh <bijh@tsingyun.net>
2020-09-27 15:58:57 +0800

    "dfs"

--- src/main/java/Amazon/Factorization/Solution2.java
+++ src/main/java/Amazon/Factorization/Solution2.java
@@ -21,12 +21,10 @@ public class Solution2 {
     public List<List<Integer>> getFactors(int n) {
 
         List<List<Integer>> ans = new ArrayList<>();
-
         List<Integer> path = new ArrayList<>();
-
         dfs(2, 1, n, path, ans);
-
         return ans;
+
     }
 
     void dfs(int start, int product, int n, List<Integer> path, List<List<Integer>> ans) {
@@ -34,24 +32,21 @@ public class Solution2 {
         if (start > n || product > n) {
             return;
         }
-
         if (product == n) {
             ans.add(new ArrayList<>(path));
             return;
         }
-
         for (int i = start; i < n; i++) {
             if (i * product > n) {
                 break;
             }
-
             if (n % i == 0) {
                 List<Integer> newPath = new ArrayList<>(path);
                 newPath.add(i);
                 dfs(i, i * product, n, newPath, ans);
             }
         }
-    }
 
+    }
 
 }
