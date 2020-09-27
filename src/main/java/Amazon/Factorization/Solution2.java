bc016c0369240028437a88ab2fe33d0270f2dd63
bijh <bijh@tsingyun.net>
2020-09-27 15:56:35 +0800

    "dfs"

--- src/main/java/Amazon/Factorization/Solution2.java
+++ src/main/java/Amazon/Factorization/Solution2.java
@@ -7,6 +7,8 @@ import java.util.List;
 /**
  * @author bijh@tsingyun.net
  * @date 2020/9/27 3:12 PM
+ *
+ * https://www.programcreek.com/2014/07/leetcode-factor-combinations-java/
  */
 public class Solution2 {
 
@@ -16,24 +18,31 @@ public class Solution2 {
 
         List<Integer> path = new ArrayList<>();
 
-        dfs(2, n, path, ans);
+        dfs(2, 1, n, path, ans);
 
         return ans;
     }
 
-    void dfs(int start, int remain, List<Integer> path, List<List<Integer>> ans) {
+    void dfs(int start, int product, int n, List<Integer> path, List<List<Integer>> ans) {
 
-        if (!path.isEmpty()) {
-            path.add(remain);
+        if (start > n || product > n) {
+            return;
+        }
+
+        if (product == n) {
             ans.add(new ArrayList<>(path));
             return;
         }
 
-        for (int i = start; i < remain / i; i++) {
-            if (remain % i == 0) {
+        for (int i = start; i < n; i++) {
+            if (i * product > n) {
+                break;
+            }
+
+            if (n % i == 0) {
                 List<Integer> newPath = new ArrayList<>(path);
-                newPath.add(start);
-                dfs(i, remain / i, newPath, ans);
+                newPath.add(i);
+                dfs(i, i * product, n, newPath, ans);
             }
         }
     }
