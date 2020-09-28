35ddfe59d5951db271110d7fc95749eab21fd405
bijh <bijh@tsingyun.net>
2020-09-28 09:25:39 +0800

    "和大于S的最小子数组"

--- src/main/java/Amazon/MinimumWindowSubstring/Solution.java
+++ src/main/java/Amazon/MinimumWindowSubstring/Solution.java
@@ -1,32 +1,44 @@
 package Amazon.MinimumWindowSubstring;
+
+
 public class Solution {
+
     //验证子串是否包含目标串
-    public Boolean vaild(int[] sourceMap, int[] targetMap) {
+    public Boolean valid(int[] sourceMap, int[] targetMap) {
+
         for (int i = 0; i < targetMap.length; i++) {
             if (targetMap[i] > sourceMap[i]) {
                 return false;
             }
         }
+
         return true;
+
     }
+
     //初始化TargetMap
     public void initTargetMap(int[] targetMap, String target) {
+
         for (int i = 0; i < target.length(); i++) {
             targetMap[target.charAt(i)]++;
         }
+
     }
+
     public String minWindow(String source, String target) {
-        int i, j = 0,ans = Integer.MAX_VALUE;
+
+        int i, j = 0, ans = Integer.MAX_VALUE;
         String minStr = "";
         int[] sourceMap = new int[256];
         int[] targetMap = new int[256];
         initTargetMap(targetMap, target);
+
         for (i = 0; i < source.length(); i++) {
-            while (j < source.length() && !vaild(sourceMap, targetMap)) {
+            while (j < source.length() && !valid(sourceMap, targetMap)) {
                 sourceMap[source.charAt(j)]++;
                 j++;
             }
-            if (vaild(sourceMap, targetMap)) {
+            if (valid(sourceMap, targetMap)) {
                 if (ans > j - i) {
                     ans = j - i;
                     minStr = source.substring(i, j);
@@ -34,6 +46,9 @@ public class Solution {
             }
             sourceMap[source.charAt(i)]--;
         }
+
         return minStr;
+
     }
+
 }
